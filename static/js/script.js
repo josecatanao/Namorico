dbeb6e4afd408633f5f2fcb9b2fa927124bfb8f3
jose catanao <josecatanaoneto@gmail.com>
2021-05-11 14:49:14 -0300

    layout finalizado

---
+++ static/js/script.js
@@ -0,0 +1,803 @@
+function Teste() {
+
+	//Intesidade da violencia no namoro
+	Intensidade_Material = 1;
+	Intensidade_Psicologica = 2;
+	Intensidade_Moral = 3;
+	Intensidade_Fisica = 4;
+
+	//Bloco de escolhas numero um
+	if(document.getElementById('1').checked) {
+		var a1 = 1;
+		var p1 = a1 * Intensidade_Fisica
+ 	}
+
+ 	else if(document.getElementById('2').checked) {
+		var a2 = 2;
+		var p1 = a2 * Intensidade_Fisica
+	}
+
+	else if(document.getElementById('3').checked) {
+		var a3 = 3;
+		var p1 = a3 * Intensidade_Fisica
+	}
+
+	else if(document.getElementById('4').checked) {
+		var a4 = 4;
+		var p1 = a4 * Intensidade_Fisica
+	}
+
+	else if(document.getElementById('5').checked) {
+		var a5 = 5;
+		var p1 = a5 * Intensidade_Fisica
+	}
+
+	//Definindo o valor do bloco um
+	p_1 = p1;
+	//console.log(p_1)
+
+
+	//Bloco de escolhas numero dois
+	if(document.getElementById('1.1').checked) {
+		var b1 = 1;
+		var p2 = b1 * Intensidade_Fisica		
+ 	}
+
+ 	else if(document.getElementById('2.1').checked) {
+		var b2 = 2;
+		var p2 = b2 * Intensidade_Fisica
+	}
+
+
+	else if(document.getElementById('3.1').checked) {
+		var b3 = 3;
+		var p2 = b3 * Intensidade_Fisica
+	}
+
+	else if(document.getElementById('4.1').checked) {
+		var b4 = 4;
+		var p2 = b4 * Intensidade_Fisica
+	}
+
+	else if(document.getElementById('5.1').checked) {
+		var b5 = 5;
+		var p2 = b5 * Intensidade_Fisica
+	}
+
+	//Definindo o valor do bloco dois
+	p_2 = p2
+	//console.log(p_2)
+
+	//Bloco de escolhas numero tres
+	if(document.getElementById('1.2').checked) {
+		var c1 = 1;
+		var p3 = c1 * Intensidade_Fisica		
+ 	}
+
+ 	else if(document.getElementById('2.2').checked) {
+		var c2 = 2;
+		var p3 = c2 * Intensidade_Fisica
+	}
+
+
+	else if(document.getElementById('3.2').checked) {
+		var c3 = 3;
+		var p3 = c3 * Intensidade_Fisica
+	}
+
+	else if(document.getElementById('4.2').checked) {
+		var c4 = 4;
+		var p3 = c4 * Intensidade_Fisica
+	}
+
+	else if(document.getElementById('5.2').checked) {
+		var c5 = 5;
+		var p3 = c5 * Intensidade_Fisica
+	}
+
+	//Definindo o valor do bloco tres
+	p_3 = p3
+	//console.log(p_3)
+
+	//Bloco de escolhas numero quatro
+	if(document.getElementById('1.3').checked) {
+		var d1 = 1;
+		var p4 = d1 * Intensidade_Fisica		
+ 	}
+
+ 	else if(document.getElementById('2.3').checked) {
+		var d2 = 2;
+		var p4 = d2 * Intensidade_Fisica
+	}
+
+
+	else if(document.getElementById('3.3').checked) {
+		var d3 = 3;
+		var p4 = d3 * Intensidade_Fisica
+	}
+
+	else if(document.getElementById('4.3').checked) {
+		var d4 = 4;
+		var p4 = d4 * Intensidade_Fisica
+	}
+
+	else if(document.getElementById('5.3').checked) {
+		var d5 = 5;
+		var p4 = d5 * Intensidade_Fisica
+	}
+
+	//Definindo o valor do bloco quatro
+	p_4 = p4
+	//console.log(p_4)
+
+	//Bloco de escolhas numero cinco
+	if(document.getElementById('1.4').checked) {
+		var e1 = 1;
+		var p5 = e1 * Intensidade_Fisica		
+ 	}
+
+ 	else if(document.getElementById('2.4').checked) {
+		var e2 = 2;
+		var p5 = e2 * Intensidade_Fisica
+	}
+
+
+	else if(document.getElementById('3.4').checked) {
+		var e3 = 3;
+		var p5 = e3 * Intensidade_Fisica
+	}
+
+	else if(document.getElementById('4.4').checked) {
+		var e4 = 4;
+		var p5 = e4 * Intensidade_Fisica
+	}
+
+	else if(document.getElementById('5.4').checked) {
+		var e5 = 5;
+		var p5 = e5 * Intensidade_Fisica
+	}
+
+	//Definindo o valor do bloco cinco
+	p_5 = p5
+	//console.log(p_5 )
+
+	//Bloco de escolhas numero seis
+	if(document.getElementById('1.5').checked) {
+		var f1 = 1;
+		var p6 = f1 * Intensidade_Fisica		
+ 	}
+
+ 	else if(document.getElementById('2.5').checked) {
+		var f2 = 2;
+		var p6 = f2 * Intensidade_Fisica
+	}
+
+
+	else if(document.getElementById('3.5').checked) {
+		var f3 = 3;
+		var p6 = f3 * Intensidade_Fisica
+	}
+
+	else if(document.getElementById('4.5').checked) {
+		var f4 = 4;
+		var p6 = f4 * Intensidade_Fisica
+	}
+
+	else if(document.getElementById('5.5').checked) {
+		var f5 = 5;
+		var p6 = f5 * Intensidade_Fisica
+	}
+
+	//Definindo o valor do bloco seis
+	p_6 = p6
+	//console.log(p_6 )
+
+	//Bloco de escolhas numero sete
+	if(document.getElementById('1.6').checked) {
+		var g1 = 1;
+		var p7 = g1 * Intensidade_Moral		
+ 	}
+
+ 	else if(document.getElementById('2.6').checked) {
+		var g2 = 2;
+		var p7 = g2 * Intensidade_Moral
+	}
+
+
+	else if(document.getElementById('3.6').checked) {
+		var g3 = 3;
+		var p7 = g3 * Intensidade_Moral
+	}
+
+	else if(document.getElementById('4.6').checked) {
+		var g4 = 4;
+		var p7 = g4 * Intensidade_Moral
+	}
+
+	else if(document.getElementById('5.6').checked) {
+		var g5 = 5;
+		var p7 = g5 * Intensidade_Moral
+	}
+
+	//Definindo o valor do bloco sete
+	p_7 = p7
+	//console.log(p_7)
+
+	//Bloco de escolhas numero oito
+	if(document.getElementById('1.7').checked) {
+		var h1 = 1;
+		var p8 = h1 * Intensidade_Moral		
+ 	}
+
+ 	else if(document.getElementById('2.7').checked) {
+		var h2 = 2;
+		var p8 = h2 * Intensidade_Moral
+	}
+
+
+	else if(document.getElementById('3.7').checked) {
+		var h3 = 3;
+		var p8 = h3 * Intensidade_Moral
+	}
+
+	else if(document.getElementById('4.7').checked) {
+		var h4 = 4;
+		var p8 = h4 * Intensidade_Moral
+	}
+
+	else if(document.getElementById('5.7').checked) {
+		var h5 = 5;
+		var p8 = h5 * Intensidade_Moral
+	}
+
+	//Definindo o valor do bloco oito
+	p_8 = p8
+	//console.log(p_8)
+
+	//Bloco de escolhas numero nove
+	if(document.getElementById('1.8').checked) {
+		var i1 = 1;
+		var p9 = i1 * Intensidade_Moral		
+ 	}
+
+ 	else if(document.getElementById('2.8').checked) {
+		var i2 = 2;
+		var p9 = i2 * Intensidade_Moral
+	}
+
+
+	else if(document.getElementById('3.8').checked) {
+		var i3 = 3;
+		var p9 = i3 * Intensidade_Moral
+	}
+
+	else if(document.getElementById('4.8').checked) {
+		var i4 = 4;
+		var p9 = i4 * Intensidade_Moral
+	}
+
+	else if(document.getElementById('5.8').checked) {
+		var i5 = 5;
+		var p9 = i5 * Intensidade_Moral
+	}
+
+	//Definindo o valor do bloco nove
+	p_9 = p9
+	//console.log(p_9)
+
+	//Bloco de escolhas numero dez
+	if(document.getElementById('1.9').checked) {
+		var j1 = 1;
+		var p10 = j1 * Intensidade_Moral		
+ 	}
+
+ 	else if(document.getElementById('2.9').checked) {
+		var j2 = 2;
+		var p10 = j2 * Intensidade_Moral
+	}
+
+
+	else if(document.getElementById('3.9').checked) {
+		var j3 = 3;
+		var p10 = j3 * Intensidade_Moral
+	}
+
+	else if(document.getElementById('4.9').checked) {
+		var j4 = 4;
+		var p10 = j4 * Intensidade_Moral
+	}
+
+	else if(document.getElementById('5.9').checked) {
+		var j5 = 5;
+		var p10 = j5 * Intensidade_Moral
+	}
+
+	//Definindo o valor do bloco dez
+	p_10 = p10
+	//console.log(p_10)
+
+	//Bloco de escolhas numero onze
+	if(document.getElementById('1.10').checked) {
+		var k1 = 1;
+		var p11 = k1 * Intensidade_Psicologica		
+ 	}
+
+ 	else if(document.getElementById('2.10').checked) {
+		var k2 = 2;
+		var p11 = k2 * Intensidade_Psicologica
+	}
+
+
+	else if(document.getElementById('3.10').checked) {
+		var k3 = 3;
+		var p11 = k3 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('4.10').checked) {
+		var k4 = 4;
+		var p11 = k4 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('5.10').checked) {
+		var k5 = 5;
+		var p11 = k5 * Intensidade_Psicologica
+	}
+
+	//Definindo o valor do bloco onze
+	p_11 = p11
+	//console.log(p_11)
+
+	//Bloco de escolhas numero doze
+	if(document.getElementById('1.11').checked) {
+		var l1 = 1;
+		var p12 = l1 * Intensidade_Psicologica		
+ 	}
+
+ 	else if(document.getElementById('2.11').checked) {
+		var l2 = 2;
+		var p12 = l2 * Intensidade_Psicologica
+	}
+
+
+	else if(document.getElementById('3.11').checked) {
+		var l3 = 3;
+		var p12 = l3 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('4.11').checked) {
+		var l4 = 4;
+		var p12 = l4 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('5.11').checked) {
+		var l5 = 5;
+		var p12 = l5 * Intensidade_Psicologica
+	}
+
+	//Definindo o valor do bloco doze
+	p_12 = p12
+	//console.log(p_12)
+
+	//Bloco de escolhas numero treze
+	if(document.getElementById('1.12').checked) {
+		var m1 = 1;
+		var p13 = m1 * Intensidade_Psicologica		
+ 	}
+
+ 	else if(document.getElementById('2.12').checked) {
+		var m2 = 2;
+		var p13 = m2 * Intensidade_Psicologica
+	}
+
+
+	else if(document.getElementById('3.12').checked) {
+		var m3 = 3;
+		var p13 = m3 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('4.12').checked) {
+		var m4 = 4;
+		var p13 = m4 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('5.12').checked) {
+		var m5 = 5;
+		var p13 = m5 * Intensidade_Psicologica
+	}
+
+	//Definindo o valor do bloco treze
+	p_13 = p13
+	//console.log(p_13)
+
+	//Bloco de escolhas numero quatorze
+	if(document.getElementById('1.13').checked) {
+		var n1 = 1;
+		var p14 = n1 * Intensidade_Psicologica		
+ 	}
+
+ 	else if(document.getElementById('2.13').checked) {
+		var n2 = 2;
+		var p14 = n2 * Intensidade_Psicologica
+	}
+
+
+	else if(document.getElementById('3.13').checked) {
+		var n3 = 3;
+		var p14 = n3 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('4.13').checked) {
+		var n4 = 4;
+		var p14 = n4 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('5.13').checked) {
+		var n5 = 5;
+		var p14 = n5 * Intensidade_Psicologica
+	}
+
+	//Definindo o valor do bloco quatorze
+	p_14 = p14
+	//console.log(p_14)
+
+	//Bloco de escolhas numero quinze
+	if(document.getElementById('1.14').checked) {
+		var o1 = 1;
+		var p15 = o1 * Intensidade_Psicologica		
+ 	}
+
+ 	else if(document.getElementById('2.14').checked) {
+		var o2 = 2;
+		var p15 = o2 * Intensidade_Psicologica
+	}
+
+
+	else if(document.getElementById('3.14').checked) {
+		var o3 = 3;
+		var p15 = o3 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('4.14').checked) {
+		var o4 = 4;
+		var p15 = o4 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('5.14').checked) {
+		var o5 = 5;
+		var p15 = o5 * Intensidade_Psicologica
+	}
+
+	//Definindo o valor do bloco quinze
+	p_15 = p15
+	//console.log(p_15)
+
+	//Bloco de escolhas numero dezeseis
+	if(document.getElementById('1.15').checked) {
+		var p1 = 1;
+		var p16 = p1 * Intensidade_Psicologica		
+ 	}
+
+ 	else if(document.getElementById('2.15').checked) {
+		var p2 = 2;
+		var p16 = p2 * Intensidade_Psicologica
+	}
+
+
+	else if(document.getElementById('3.15').checked) {
+		var p3 = 3;
+		var p16 = p3 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('4.15').checked) {
+		var p4 = 4;
+		var p16 = p4 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('5.15').checked) {
+		var p5 = 5;
+		var p16 = p5 * Intensidade_Psicologica
+	}
+
+	//Definindo o valor do bloco dezeseis
+	p_16 = p16
+	//console.log(p_16)
+
+	//Bloco de escolhas numero dezesete
+	if(document.getElementById('1.16').checked) {
+		var q1 = 1;
+		var p17 = q1 * Intensidade_Psicologica		
+ 	}
+
+ 	else if(document.getElementById('2.16').checked) {
+		var q2 = 2;
+		var p17 = q2 * Intensidade_Psicologica
+	}
+
+
+	else if(document.getElementById('3.16').checked) {
+		var q3 = 3;
+		var p17 = q3 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('4.16').checked) {
+		var q4 = 4;
+		var p17 = q4 * Intensidade_Psicologica
+	}
+
+	else if(document.getElementById('5.16').checked) {
+		var q5 = 5;
+		var p17 = q5 * Intensidade_Psicologica
+	}
+
+	//Definindo o valor do bloco dezesete
+	p_17 = p17
+	//console.log(p_17)
+
+	//Bloco de escolhas numero dezeoito
+	if(document.getElementById('1.17').checked) {
+		var r1 = 1;
+		var p18 = r1 * Intensidade_Material	
+ 	}
+
+ 	else if(document.getElementById('2.17').checked) {
+		var r2 = 2;
+		var p18 = r2 * Intensidade_Material
+	}
+
+
+	else if(document.getElementById('3.17').checked) {
+		var r3 = 3;
+		var p18 = r3 * Intensidade_Material
+	}
+
+	else if(document.getElementById('4.17').checked) {
+		var r4 = 4;
+		var p18 = r4 * Intensidade_Material
+	}
+
+	else if(document.getElementById('5.17').checked) {
+		var r5 = 5;
+		var p18 = r5 * Intensidade_Material
+	}
+
+	//Definindo o valor do bloco dezeoito
+	p_18 = p18
+	//console.log(p_18)
+
+	//Bloco de escolhas numero dezenove
+	if(document.getElementById('1.18').checked) {
+		var s1 = 1;
+		var p19 = s1 * Intensidade_Material		
+ 	}
+
+ 	else if(document.getElementById('2.18').checked) {
+		var s2 = 2;
+		var p19 = s2 * Intensidade_Material
+	}
+
+
+	else if(document.getElementById('3.18').checked) {
+		var s3 = 3;
+		var p19 = s3 * Intensidade_Material
+	}
+
+	else if(document.getElementById('4.18').checked) {
+		var s4 = 4;
+		var p19 = s4 * Intensidade_Material
+	}
+
+	else if(document.getElementById('5.18').checked) {
+		var s5 = 5;
+		var p19 = s5 * Intensidade_Material
+	}
+
+	//Definindo o valor do bloco dezenove
+	p_19 = p19
+	//console.log(p_19)
+
+	//Bloco de escolhas numero vinte
+	if(document.getElementById('1.19').checked) {
+		var t1 = 1;
+		var p20 = t1 * Intensidade_Material		
+ 	}
+
+ 	else if(document.getElementById('2.19').checked) {
+		var t2 = 2;
+		var p20 = t2 * Intensidade_Material
+	}
+
+
+	else if(document.getElementById('3.19').checked) {
+		var t3 = 3;
+		var p20 = t3 * Intensidade_Material
+	}
+
+	else if(document.getElementById('4.19').checked) {
+		var t4 = 4;
+		var p20 = t4 * Intensidade_Material
+	}
+
+	else if(document.getElementById('5.19').checked) {
+		var t5 = 5;
+		var p20 = t5 * Intensidade_Material
+	}
+
+	//Definindo o valor do bloco vinte
+	p_20 = p20
+	//console.log(p_20)
+
+	//Bloco de escolhas numero vinte um
+	if(document.getElementById('1.20').checked) {
+		var u1 = 1;
+		var p21 = u1 * Intensidade_Material		
+ 	}
+
+ 	else if(document.getElementById('2.20').checked) {
+		var u2 = 2;
+		var p21 = u2 * Intensidade_Material
+	}
+
+
+	else if(document.getElementById('3.20').checked) {
+		var u3 = 3;
+		var p21 = u3 * Intensidade_Material
+	}
+
+	else if(document.getElementById('4.20').checked) {
+		var u4 = 4;
+		var p21 = u4 * Intensidade_Material
+	}
+
+	else if(document.getElementById('5.20').checked) {
+		var u5 = 5;
+		var p21 = u5 * Intensidade_Material
+	}
+
+	//Definindo o valor do bloco vinte um
+	p_21 = p21
+	//console.log(p_21)
+
+	//Bloco de escolhas numero vinte dois
+	if(document.getElementById('1.21').checked) {
+		var v1 = 1;
+		var p22 = v1 * Intensidade_Material		
+ 	}
+
+ 	else if(document.getElementById('2.21').checked) {
+		var v2 = 2;
+		var p22 = v2 * Intensidade_Material
+	}
+
+
+	else if(document.getElementById('3.21').checked) {
+		var v3 = 3;
+		var p22 = v3 * Intensidade_Material
+	}
+
+	else if(document.getElementById('4.21').checked) {
+		var v4 = 4;
+		var p22 = v4 * Intensidade_Material
+	}
+
+	else if(document.getElementById('5.21').checked) {
+		var v5 = 5;
+		var p22 = v5 * Intensidade_Material
+	}
+
+	//Definindo o valor do bloco vinte dois
+	p_22 = p22
+	//console.log(p_22)
+
+	//Bloco de escolhas numero vinte tres
+	if(document.getElementById('1.22').checked) {
+		var w1 = 1;
+		var p23 = w1 * Intensidade_Material		
+ 	}
+
+ 	else if(document.getElementById('2.22').checked) {
+		var w2 = 2;
+		var p23 = w2 * Intensidade_Material
+	}
+
+
+	else if(document.getElementById('3.22').checked) {
+		var w3 = 3;
+		var p23 = w3 * Intensidade_Material
+	}
+
+	else if(document.getElementById('4.22').checked) {
+		var w4 = 4;
+		var p23 = w4 * Intensidade_Material
+	}
+
+	else if(document.getElementById('5.22').checked) {
+		var w5 = 5;
+		var p23 = w5 * Intensidade_Material
+	}
+
+	//Definindo o valor do bloco vinte tres
+	p_23 = p23
+	//console.log(p_23)
+
+	//Bloco de escolhas numero vinte quatro
+	if(document.getElementById('1.23').checked) {
+		var x1 = 1;
+		var p24 = x1 * Intensidade_Material		
+ 	}
+
+ 	else if(document.getElementById('2.23').checked) {
+		var x2 = 2;
+		var p24 = x2 * Intensidade_Material
+	}
+
+
+	else if(document.getElementById('3.23').checked) {
+		var x3 = 3;
+		var p24 = x3 * Intensidade_Material
+	}
+
+	else if(document.getElementById('4.23').checked) {
+		var x4 = 4;
+		var p24 = x4 * Intensidade_Material
+	}
+
+	else if(document.getElementById('5.23').checked) {
+		var x5 = 5;
+		var p24 = x5 * Intensidade_Material
+	}
+
+	//Definindo o valor do bloco vinte quatro
+	p_24 = p24
+
+	//obtendo o resultados
+
+	violencia_fisica = (p_1 + p_2 + p_3 + p_4 + p_5 + p_6 )
+	violencia_fisica = (violencia_fisica / 6)
+	//alert(violencia_fisica)
+
+	violencia_moral = (p_7 + p_8 + p_9 + p_10)
+	violencia_moral = (violencia_moral / 4)
+	//alert(violencia_moral)
+
+	resultado = ( p_1 + p_2 + p_3 + p_4 + p_5 + p_6 + p_7 + p_8 + p_9 + p_10 + p_11 + p_12 + p_13 + p_14 + p_15 + p_16 + p_17 + p_18 + p_19 + p_20 + p_21 + p_22 + p_23 + p_24 )
+	resultado = (resultado / 24)
+	//alert(resultado)
+	//alert(p_1)
+
+	// Classificação da qualidade de namoro
+
+			if(violencia_fisica > 4 && violencia_fisica < 5 || violencia_moral > 3 && violencia_moral < 4){
+				window.location.href = "Intensidade.IV.html";
+				//document.write("Relação violenta")
+			}
+
+			else if(violencia_fisica > 5 || violencia_moral > 4){
+				window.location.href = "Intensidade.V.html";
+				//document.write("Relação extremamente violenta")
+			}
+
+			else if(resultado > 2.37 && resultado < 2.38){ 
+				window.location.href = "Intensidade.I.html";
+			}
+
+			else if(resultado >= 2.38 && resultado <= 4.75){
+				window.location.href = "Intensidade.II.html";
+				//document.write("Relação com sinais isolados de violência")
+			}
+
+			else if(resultado > 4.75 && resultado < 7.13){
+				window.location.href = "Intensidade.III.html";
+				//document.write("Relação com sinais frequentes de violência")
+			}
+
+			else if(resultado >= 7.13 && resultado <= 9.5 ){
+				window.location.href = "Intensidade.IV.html";
+				//document.write("Relação violenta")
+			}
+
+			else if(resultado > 9.5 && resultado <= 11.9){
+				window.location.href = "Intensidade.V.html";
+				//document.write("Relação extremamente violenta")
+			}
+
+ }
